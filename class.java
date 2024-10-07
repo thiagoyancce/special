326a880ad7cae263bdda0e3f8978bf8305b82873
thiagoyancce <31606885+thiagoyancce@users.noreply.github.com>
2024-10-07 18:32:25 -0500

    Create class.java

---
+++ class.java
@@ -0,0 +1,6 @@
+public class PruebaCaracteres {
+    public static void main(String[] args) {
+        String texto = "Hola, ¿cómo estás? ¡Aquí está la ñ y los acentos: á, é, í, ó, ú!";
+        System.out.println(texto);
+    }
+}
